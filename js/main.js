09bd59ac6c8c89daf47fddccfe44f5ac9cead881
anouther <155570107+anouther@users.noreply.github.com>
2025-05-21 21:53:54 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('happiest birthday, my one and only.').split('');
+        const titles = ('Happy 22nd birthday, love.').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "thank you for being born, sayang. Even words can’t describe how grateful i am to have you in my life.";
+            subtitle.innerText = "You're 22 now, and I know you carry so many dreams, worries, and hopes. I just want you to know: I see you. All of you. And I'm proud of how far you've come. No matter where life takes us, I’ll always be your quiet cheerleader, your loudest fan, and your safe place to come home to. So grow at your own pace. Dream big. Fail, get up, and try again. I’ll be right here, loving you with everything I’ve got.";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
